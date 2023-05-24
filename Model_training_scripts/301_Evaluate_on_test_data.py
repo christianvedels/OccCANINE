fbffa20cb33eb775a21e5c1e5ac094f57d372f53
christianvedels <christian-vs@sam.sdu.dk>
2023-05-24 12:49:05 +0200

    Update 301_Evaluate_on_test_data.py

--- Model_training_scripts/301_Evaluate_on_test_data.py
+++ Model_training_scripts/301_Evaluate_on_test_data.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Evaluate on validation
+Evaluate on test data
 Created on Tue May 23 15:02:16 2023
 
 Authors: Christian Vedel, [christian-vs@sam.sdu.dk],
 
-Purpose: Evaluates performance of the models on validation data
+Purpose: Evaluates performance of the models on test data
 """
 
