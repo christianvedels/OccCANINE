9b1491b475c9c479041a70f36f497d7256644654
christianvedels <christian-vs@sam.sdu.dk>
2023-08-03 14:30:33 +0200

    Update 002_XML_RoBERTa_Model.py
    First version that works

--- Model_training_scripts/002_XML_RoBERTa_Model.py
+++ Model_training_scripts/002_XML_RoBERTa_Model.py
@@ -2,23 +2,39 @@
 """
 Created on Wed Aug  2 09:58:40 2023
 
+https://huggingface.co/papluca/xlm-roberta-base-language-detection
+https://colab.research.google.com/drive/15LJTckS6gU3RQOmjLqxVNBmbsBdnUEvl?usp=sharing#scrollTo=V_gbHRmNHEWU
+
+https://github.com/abhimishra91/transformers-tutorials/blob/master/transformers_multi_label_classification.ipynb
+
 @author: christian-vs
 """
 
 # %% Parameters
 n_epochs = 10 # The networks trains a maximum of these epochs
 
-sample_size = 4
+sample_size = 3
 batch_size = 16
 
 # %% Import packages
 import pandas as pd
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler, Dataset 
 import os
 import random as r
-from transformers import AutoTokenizer
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForSequenceClassification, 
+    DataCollatorWithPadding, 
+    pipeline,
+    Trainer,
+    TrainingArguments
+)
+import transformers
+from sklearn.metrics import f1_score, accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # %% Check cuda availability
 is_cuda = torch.cuda.is_available()
@@ -29,7 +45,7 @@ else:
     device = torch.device("cpu")
     print("GPU not available, CPU used")
 
-# device = torch.device("cpu")    
+device = torch.device("cpu")    
 
 #%% Load data
 if os.environ['COMPUTERNAME'] == 'SAM-126260':
@@ -41,250 +57,274 @@ print(os.getcwd())
 fname = "Data/Training_data/DK_census_train.csv"
 df = pd.read_csv(fname, encoding = "UTF-8")
 
-# Subset to larger toy data
-r.seed(20)
-df = df.sample(10**sample_size)
+key = pd.read_csv("Data/Key.csv") # Load key and convert to dictionary
+key = key[1:]
+key = zip(key.code, key.hisco)
+key = list(key)
+key = dict(key)
+label2id = key
+id2label = {v: k for k, v in label2id.items()}
+ 
+
+# Subset to smaller
+if(10**sample_size < df.shape[0]):
+    r.seed(20)
+    df = df.sample(10**sample_size)
+
+# Remove no occ
+# df = df[df.code1==2]
+
+# Split into internal test/train/val
+train_ratio = 0.75
+validation_ratio = 0.15
+test_ratio = 0.10
+
+# train is now 75% of the entire data set
+df_train, df_test = train_test_split(df, test_size=1 - train_ratio)
+
+# test is now 10% of the initial data set
+# validation is now 15% of the initial data set
+df_val, df_test = train_test_split(df_test, test_size=test_ratio/(test_ratio + validation_ratio)) 
+print(f"Train / valid / test samples: {len(df_train)} / {len(df_val)} / {len(df_test)}")
+
+df_train = df_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+# %% Labels to list
+# Each person can have up to 5 occupations. This part converts this into binary representation in a NxK matrix
+def labels_to_bin(df, max_value):
+    df_codes = df[["code1", "code2", "code3", "code4", "code5"]]
+
+    # Binarize
+    labels_list = df_codes.values.tolist()
+
+    # == Build outcome matrix ==
+    # Convert the given list to a NumPy array
+    labels_array = np.array(labels_list)
+
+    # Find the maximum integer value in the given array
+    max_value = max_value
+
+    # Construct the NxK matrix
+    N = len(labels_list)
+    K = len(label2id)
+    labels = np.zeros((N, K), dtype=int)
+
+    for i, row in enumerate(labels_list):
+        for value in row:
+            if not np.isnan(value):
+                labels[i, int(value)] = 1
 
 
-# # %% Resample
-# # Count the number of occurrences for each class in 'code1', 'code2', 'code3', 'code4', 'code5'
-# all_class_columns = ['code1', 'code2', 'code3', 'code4', 'code5']
-# class_counts = df[all_class_columns].stack().value_counts()
+    # The columns 0-2 contains all those labelled as having 'no occupation'.
+    # But since any individuals is encoded with up 5 occupations, and any one o
+    # these can be 'no occupation' it occurs erroneously with high frequency.
+    # Fix: Check if any other occupation is positve.
+    # Iterate through each row of the array
+    for row in labels:
+        # Check if the value in the first column is '1'
+        if row[2] == 1 | row[1] == 1 | row[0] == 1:
+            # Check if there are any positive values in the remaining row (excluding the first column)
+            if np.any(row[3:]>0):
+                # Set the first column to '0' if positive values are found
+                row[0] = 0
 
-# # Identify the classes with occurrences less than 1000 and more than 10% of the data
-# classes_less_than_1000 = class_counts.index[class_counts < 1000]
-# classes_more_than_10_percent = class_counts.index[class_counts > df.shape[0] * 0.1]
-
-# for i in classes_less_than_1000:
-#     i
-
-
+    labels = labels.astype(float)
+    
+    # Convert each row of the array to a 1D list
+    # labels = [row.tolist() for row in labels]
+    
+    return(labels)
 
 
-
-# # Upsample classes with occurrences less than 1000 to 1000 samples
-# for col in ['code1', 'code2', 'code3', 'code4', 'code5']:
-#     mask = df[col].isin(classes_less_than_1000)  # Filter rows where the class code is in small classes
-#     small_class_indices = df[mask].index.tolist()  # Get indices of rows with small classes
-#     upsampled_rows = df.loc[r.choices(small_class_indices, k=1000 - len(small_class_indices))]
-#     df = pd.concat([df, upsampled_rows])
-
-# # Downsample classes with occurrences more than 10% to 10% of the data
-# for col in ['code1', 'code2', 'code3', 'code4', 'code5']:
-#     mask = df[col].isin(classes_more_than_10_percent)  # Filter rows where the class code is in large classes
-#     large_class_indices = df[mask].index.tolist()  # Get indices of rows with large classes
-#     downsampled_rows = df.loc[r.sample(large_class_indices, k=int(df.shape[0] * 0.1))]
-#     df = pd.concat([df, downsampled_rows])
-
-# # Shuffle the dataframe to ensure the order is random
-# df = df.sample(frac=1).reset_index(drop=True)
-
+# Run 
+max_val = len(key)
+labels_bin = labels_to_bin(df, max_value = max_val)
+labels_train = labels_to_bin(df_train, max_value = max_val)
+labels_val = labels_to_bin(df_val, max_value = max_val)
+labels_test = labels_to_bin(df_test, max_value = max_val)
 
 #%% Tokenizer
 tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
 
-# Now we need to update the tokenizer with words it has not seen before
-# List of unique words
-all_text = ' '.join(df.occ1.tolist())
-words_list = all_text.split()
-unique_words = set(words_list)
+# %% Key vars
+MAX_LEN = 200
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+LEARNING_RATE = 1e-05
 
-# Add tokens for missing words
-tokenizer.add_tokens(list(unique_words))
+# %% Defining CustomDataset 
+# (https://github.com/abhimishra91/transformers-tutorials/blob/master/transformers_multi_label_classification.ipynb)
 
-# Testing tokenizer
-print({x : tokenizer.encode(x, add_special_tokens=False) for x in df.occ1.values[0].split()})
+class CustomDataset(Dataset):
 
-# Tokenize features
-MAX_LEN = 128
-features = df.occ1.values.tolist()
-tokenized_feature = tokenizer.batch_encode_plus(
-                            # Sentences to encode
-                            features, 
-                            # Add '[CLS]' and '[SEP]'
-                            add_special_tokens = True,
-                            # Add empty tokens if len(text)<MAX_LEN
-                            padding = 'max_length',
-                            # Truncate all sentences to max length
-                            truncation=True,
-                            # Set the maximum length
-                            max_length = MAX_LEN, 
-                            # Return attention mask
-                            return_attention_mask = True,
-                            # Return pytorch tensors
-                            return_tensors = 'pt'       
-                   )
+    def __init__(self, dataframe, tokenizer, max_len, Labels):
+        self.tokenizer = tokenizer
+        self._data = dataframe
+        self.occ1 = dataframe.occ1
+        self.targets = Labels
+        self.max_len = max_len
 
-# %% Preprocessesing
-tokenizer("huusmand og fisker mand")
+    def __len__(self):
+        return len(self.occ1)
 
-# MAX_LEN = 512
-MAX_LEN = 64
-tokenized_feature = tokenizer.batch_encode_plus(
-    # Sentences to encode
-    df['occ1'].tolist(),
-    # Add '[CLS]' and '[SEP]'
-    add_special_tokens = True,
-    # Add empty tokens if len(text)<MAX_LEN
-    padding = 'max_length',
-    # Truncate all sentences to max length
-    truncation=True,
-    # Set the maximum length
-    max_length = MAX_LEN, 
-    # Return attention mask
-    return_attention_mask = True,
-    # Return pytorch tensors
-    return_tensors = 'pt'       
-    )
+    def __getitem__(self, index):
+        occ1 = str(self.occ1[index])
+        occ1 = " ".join(occ1.split())
 
-# %% Labels to list
-# Each person can have up to 5 occupations
-df_codes = df[["code1", "code2", "code3", "code4", "code5"]]
+        inputs = self.tokenizer.encode_plus(
+            occ1,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
 
-# Load binarizer
-# from sklearn.preprocessing import MultiLabelBinarizer
-# one_hot_labels = MultiLabelBinarizer()
+        # breakpoint()
 
-# Binarize
-labels_list = df_codes.values.tolist()
-
-# Build outcome matrix
-# Convert the given list to a NumPy array
-labels_array = np.array(labels_list)
-
-# Find the maximum integer value in the given array
-max_value = int(np.nanmax(labels_array))
-
-# Construct the NxK matrix
-N = len(labels_list)
-K = max_value + 1
-labels = np.zeros((N, K), dtype=int)
-
-for i, row in enumerate(labels_list):
-    for value in row:
-        if not np.isnan(value):
-            labels[i, int(value)] = 1
-
-
-# The columns 0-2 contains all those labelled as having 'no occupation'.
-# But since any individuals is encoded with up 5 occupations, and any one o
-# these can be 'no occupation' it occurs erroneously with high frequency.
-# Fix: Check if any other occupation is positve.
-
-# Iterate through each row of the array
-for row in labels:
-    # Check if the value in the first column is '1'
-    if row[2] == 1 | row[1] == 1 | row[0] == 1:
-        # Check if there are any positive values in the remaining row (excluding the first column)
-        if np.any(row[3:]>0):
-            # Set the first column to '0' if positive values are found
-            row[0] = 0
-
-labels = labels.astype(float)
-
-# %% Prepare data
-from sklearn.model_selection import train_test_split
-train_inputs, validation_inputs, train_labels, validation_labels, train_masks, validation_masks = train_test_split(
-    tokenized_feature['input_ids'], labels, tokenized_feature['attention_mask'], random_state=20, test_size=0.2
-    )
-
-# Create the DataLoader for our training set
-train_data = TensorDataset(train_inputs, train_masks, torch.tensor(train_labels))
-train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-# Create the DataLoader for our test set
-validation_data = TensorDataset(validation_inputs, validation_masks, torch.tensor(validation_labels))
-validation_sampler = SequentialSampler(validation_data)
-validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
-
-# %% Define model
-from transformers import XLMRobertaForSequenceClassification, AdamW, get_linear_schedule_with_warmup
-model = XLMRobertaForSequenceClassification.from_pretrained(
-    "xlm-roberta-base", 
-    # Specify number of classes
-    num_labels = labels.shape[1], 
-    # Whether the model returns attentions weights
-    output_attentions = False,
-    # Whether the model returns all hidden-states 
-    output_hidden_states = False,
-    problem_type="multi_label_classification"
-)
-
-# Adapt model size to the tokens added:
-model.resize_token_embeddings(len(tokenizer))
-
-# Optimizer & Learning Rate Scheduler
-optimizer = AdamW(model.parameters(),
-                  lr = 3e-1, 
-                  eps = 1e-16
-                )
-
-# Number of training epochs
-epochs = n_epochs
-# Total number of training steps is number of batches * number of epochs.
-total_steps = len(df['occ1'].tolist()) * epochs
-# Create the learning rate scheduler
-scheduler = get_linear_schedule_with_warmup(optimizer,
-                                            num_warmup_steps = 0,
-                                            num_training_steps = total_steps)
-
-# tell pytorch to run this model on GPU
-model.cuda()
-# model.to('cpu')
-
-# %% Training
-
-# Training
-import time
-# Store the average loss after each epoch 
-loss_values = []
-
-# number of total steps for each epoch
-print('total steps per epoch: ',  len(df['occ1'].tolist()) / batch_size)
-# looping over epoch
-for epoch_i in range(0, epochs):
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
     
-    print('training on epoch: ', epoch_i)
-    # set start time 
-    t0 = time.time()
-    # reset total loss
-    total_loss = 0
-    # model in training 
-    model.train()
-    # loop through batch 
-    for step, batch in enumerate(train_dataloader):
-        # load data from dataloader 
-        b_input_ids = batch[0].to(device)
-        b_input_mask = batch[1].to(device)
-        b_labels = batch[2].to(device)
-        # clear any previously calculated gradients 
-        model.zero_grad()
-        # get outputs
-        outputs = model(b_input_ids,
-                        token_type_ids=None,
-                        attention_mask=b_input_mask,
-                        labels=b_labels)
-        # get loss
-        loss = outputs[0]
-        # total loss
-        total_loss += loss.item()
-        # clip the norm of the gradients to 1.0.
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        # update optimizer
-        optimizer.step()
-        # update learning rate 
-        scheduler.step()
+    
+# %% Creating the dataset and dataloader for the neural network
+
+training_set = CustomDataset(df_train, tokenizer, MAX_LEN, labels_train)
+testing_set = CustomDataset(df_val, tokenizer, MAX_LEN, labels_val)
+
+# %% Train params
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+
+# %% Model class
+
+class xmlRoBERTaClass(torch.nn.Module):
+    def __init__(self):
+        super(xmlRoBERTaClass, self).__init__()
+        self.l1 = transformers.XLMRobertaModel.from_pretrained('xlm-roberta-base')
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, len(key))
+    
+    def forward(self, ids, mask):
+        # breakpoint()
         
-        # Progress update every 10 steps
-        if step % 10 == 0 and not step == 0:
-            print(
-                'training on step: ', step, 
-                '; loss:', np.round(loss.item(), 4) ,
-                # 'total time used is: {0:.2f} s'.format(time.time() - t0),
-                end = '\n'
-                )
-    # Calculate the average loss over the training data.
-    avg_train_loss = total_loss / len(train_dataloader)
-    # Store the loss value for plotting the learning curve..
-    loss_values.append(avg_train_loss)
-    print("average training loss: {0:.2f}".format(avg_train_loss))
+        _, output_1= self.l1(ids, attention_mask = mask, return_dict=False)
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
+
+model = xmlRoBERTaClass()
+model.to(device)
+
+# %% Loss and optimizer
+def loss_fn(outputs, targets):
+    return torch.nn.BCEWithLogitsLoss()(outputs, targets)
+
+optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+
+# %% Plot progress
+def plot_progress(train_losses, val_losses, step):
+    plt.figure(figsize=(8, 6))
+    plt.plot(train_losses, color='blue', label='Training Loss')
+    plt.plot(val_losses, color='red', label='Validation Loss')
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss Progress')
+    plt.legend()
+    plt.grid(True)   
+    
+    # Create the "Tmp progress" folder if it doesn't exist
+    if not os.path.exists("Tmp traning plots"):
+       os.makedirs("Tmp traning plots")
+    # Save the plot as an image in the folder
+    plt.savefig(f"Tmp traning plots/loss_{step}.png")
+    plt.show()
+    plt.close()
+
+# %% Validation function
+def validate(model, data_loader, loss_fn):
+    model.eval()
+    total_loss = 0.0
+    num_batches = 0
+
+    with torch.no_grad():
+        for _, data in enumerate(data_loader, 0):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype=torch.float)
+
+            outputs = model(ids, mask)
+            loss = loss_fn(outputs, targets)
+
+            total_loss += loss.item()
+            num_batches += 1
+
+    avg_loss = total_loss / num_batches
+    return avg_loss
+
+# %% Fine tuning
+losses = []
+val_losses = []
+
+def train(epoch):
+    model.train()
+    
+    # Validation after each epoch
+    val_loss = validate(model, testing_loader, loss_fn)
+    print(f"Validation Loss: {val_loss}")
+    
+    for _,data in enumerate(training_loader, 0):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.float)
+
+        outputs = model(ids, mask)
+
+        optimizer.zero_grad()
+        loss = loss_fn(outputs, targets)
+        losses.append(loss.item())
+        val_losses.append(val_loss) # Add each step to have plot to follow
+        
+        # Update to console and plot
+        if _%1==0:
+            print(f'Step: {_}, Epoch: {epoch}, Loss:  {loss.item()}')
+            plot_progress(losses, val_losses, step=_)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+for epoch in range(EPOCHS):
+    train(epoch)
+
+
+
+
+
+
+
+
+
