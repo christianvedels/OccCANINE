c422c2c9f00e658017acc1b65f25735aec48753b
christianvedels <christian-vs@sam.sdu.dk>
2023-10-09 15:02:22 +0200

    Update 009_SWEPOP_data.R

--- Data_cleaning_scripts/009_SWEPOP_data.R
+++ Data_cleaning_scripts/009_SWEPOP_data.R
@@ -12,16 +12,124 @@ source("Data_cleaning_scripts/000_Functions.R")
 library(foreach)
 
 # ==== Read data ====
-data0 = read_csv2("Data/Raw_data/SWEDPOP_data/individual.csv", locale = locale(encoding = "ISO-8859-1"))
+data0 = read_csv2(
+  "Data/Raw_data/SWEDPOP_data/individual.csv", 
+  locale = locale(encoding = "ISO-8859-1")
+)
 
-for(f in list.files("Data/Raw_data/SWEDPOP_data")){
-  if(grepl(".csv", f)){
-    x = read_csv2(paste0("Data/Raw_data/SWEDPOP_data/", f), locale = locale(encoding = "ISO-8859-1"), n_max = 1000)
-    print(f)
-    print(colnames(x))
-  }
-}
+# ==== Cleaning data ====
+# data0$TYPE %>% unique()
+# [1] "ARRIVAL_FROM"                           "PERSON_IN_LABOUR_FORCE"                
+# [3] "PERSON_IS_ABSENT"                       "BIRTH_DATE"                            
+# [5] "BIRTH_LOCATION"                         "CIVIL_STATUS"                          
+# [7] "OCCUPATION_HISCO_PART_1"                "OCCUPATION_HISCO_PRODUCT_PART_1"       
+# [9] "OCCUPATION_HISCO_RELATION_PART_1"       "OCCUPATION_HISCO_STATUS_PART_1"        
+# [11] "BIRTH_RESIDENCE"                        "OCCUPATION_HISCO_PART_2"               
+# [13] "OCCUPATION_HISCO_PRODUCT_PART_2"        "OCCUPATION_HISCO_RELATION_PART_2"      
+# [15] "OCCUPATION_HISCO_STATUS_PART_2"         "REASON_FOR_BEING_ABSENT"               
+# [17] "DISABILITY_AS_TRANSCRIBED"              "DISABILITY_CODE_1"                     
+# [19] "NOT_BAPTISED"                           "TITLE_AS_TRANSCRIBED"                  
+# [21] "OCCUPATION_HISCO_PART_3"                "OCCUPATION_HISCO_PRODUCT_PART_3"       
+# [23] "OCCUPATION_HISCO_RELATION_PART_3"       "OCCUPATION_HISCO_STATUS_PART_3"        
+# [25] "DISABILITY_CODE_2"                      "BAPTISM_DATE"                          
+# [27] "OCCUPATION_HISCO_PART_4"                "OCCUPATION_HISCO_PRODUCT_PART_4"       
+# [29] "OCCUPATION_HISCO_RELATION_PART_4"       "OCCUPATION_HISCO_STATUS_PART_4"        
+# [31] "BAPTISM_LOCATION"                       "STILLBIRTH_DATE"                       
+# [33] "DISABILITY_CODE_3"                      "STILLBIRTH_LOCATION"                   
+# [35] "CHILDBIRTH_ASSISTANT"                   "HAS_LEFT_THE_SWEDISH_CHURCH"           
+# [37] "OCCUPATION_HISCO_PART_5"                "OCCUPATION_HISCO_PRODUCT_PART_5"       
+# [39] "OCCUPATION_HISCO_RELATION_PART_5"       "OCCUPATION_HISCO_STATUS_PART_5"        
+# [41] "DEATH_DATE"                             "OCCUPATION_STANDARD_PART_1"            
+# [43] "START_OBSERVATION"                      "DEPARTURE_TO"                          
+# [45] "BIRTH_OCCUPATION_HISCO_PART_1"          "BIRTH_OCCUPATION_HISCO_STATUS_PART_1"  
+# [47] "BIRTH_OCCUPATION_HISCO_RELATION_PART_1" "MARRIAGE_DATE"                         
+# [49] "END_OBSERVATION"                        "INCOME_TAXED"                          
+# [51] "BIRTH_OCCUPATION_HISCO_STATUS_PART_2"   "INCOME_AGRICULTURAL_PROPERTY"          
+# [53] "INCOME_CAPITAL"                         "INCOME_IMMOVABLE_PROPERTY"             
+# [55] "INCOME_LABOUR"                          "INCOME_SELF_EMPLOYMENT"                
+# [57] "INCOME_TOTAL"                           "DEATH_CAUSE_ICD10H_PART_1"             
+# [59] "INCOME_OTHER_PROPERTY"                  "DEATH_LOCATION"                        
+# [61] "OCCUPATION_STANDARD_PART_2"             "BIRTH_OCCUPATION_HISCO_PART_2"         
+# [63] "BIRTH_OCCUPATION_HISCO_RELATION_PART_2" "DEATH_CAUSE_ICD10H_PART_2"             
+# [65] "OCCUPATION_STANDARD_PART_3"             "DEATH_CAUSE_ICD10H_PART_3"             
+# [67] "BIRTH_OCCUPATION_HISCO_STATUS_PART_3"   "DEATH_CAUSE_ICD10H_PART_4"             
+# [69] "BIRTH_OCCUPATION_HISCO_PART_3"          "BIRTH_OCCUPATION_HISCO_RELATION_PART_3"
+# [71] "DEATH_CAUSE_ICD10H_PART_5"              "DIVORCE_DATE"                          
+# [73] "FUNERAL_DATE"                           "FUNERAL_LOCATION"                      
+# [75] "LEGITIMACY"                             "MARRIAGE_LOCATION"                     
+# [77] "MARRIAGE_SEQUENCE"                      "MULTIPLE_BIRTH"                        
+# [79] "OCCUPATION_STANDARD_PART_4"             "OCCUPATION_STANDARD_PART_5"            
+# [81] "OCCUPATION_HISCO_RELATION_PART_6"       "OCCUPATION_HISCO_STATUS_PART_6"        
+# [83] "OCCUPATION_HISCO_PART_6"                "OCCUPATION_STANDARD_PART_6"            
+# [85] "OCCUPATION_HISCO_PRODUCT_PART_6"        "VACCINATION"                           
+# [87] "DEATH_RESIDENCE" 
+data0 = data0 %>% 
+  filter(
+    TYPE %in% c(
+      "BIRTH_OCCUPATION_HISCO_PART_1",
+      "BIRTH_OCCUPATION_HISCO_PART_2",
+      "BIRTH_OCCUPATION_HISCO_PART_3",
+      
+      # # Status
+      # "BIRTH_OCCUPATION_HISCO_STATUS_PART_1",
+      # "BIRTH_OCCUPATION_HISCO_STATUS_PART_2",
+      # "BIRTH_OCCUPATION_HISCO_STATUS_PART_3",
+      # 
+      # # Relation
+      # "BIRTH_OCCUPATION_HISCO_RELATION_PART_1",
+      # "BIRTH_OCCUPATION_HISCO_RELATION_PART_2",
+      # "BIRTH_OCCUPATION_HISCO_RELATION_PART_3",
+      
+      # HISCO
+      "OCCUPATION_HISCO_PART_1",
+      "OCCUPATION_HISCO_PART_2",
+      "OCCUPATION_HISCO_PART_3",
+      "OCCUPATION_HISCO_PART_4",
+      "OCCUPATION_HISCO_PART_5",
+      "OCCUPATION_HISCO_PART_6",
+      
+      # # Status
+      # "OCCUPATION_HISCO_STATUS_PART_1",
+      # "OCCUPATION_HISCO_STATUS_PART_2",
+      # "OCCUPATION_HISCO_STATUS_PART_3",
+      # "OCCUPATION_HISCO_STATUS_PART_4",
+      # "OCCUPATION_HISCO_STATUS_PART_5",
+      # "OCCUPATION_HISCO_STATUS_PART_6",
+      # 
+      # # Product 
+      # "OCCUPATION_HISCO_PRODUCT_PART_1",
+      # "OCCUPATION_HISCO_PRODUCT_PART_2",
+      # "OCCUPATION_HISCO_PRODUCT_PART_3",
+      # "OCCUPATION_HISCO_PRODUCT_PART_4",
+      # "OCCUPATION_HISCO_PRODUCT_PART_5",
+      # "OCCUPATION_HISCO_PRODUCT_PART_6",
+      # 
+      # # Relation
+      # "OCCUPATION_HISCO_RELATION_PART_1",
+      # "OCCUPATION_HISCO_RELATION_PART_2",
+      # "OCCUPATION_HISCO_RELATION_PART_3",
+      # "OCCUPATION_HISCO_RELATION_PART_4",
+      # "OCCUPATION_HISCO_RELATION_PART_5",
+      # "OCCUPATION_HISCO_RELATION_PART_6",
+      
+      # Cleaned title
+      "OCCUPATION_STANDARD_PART_1",
+      "OCCUPATION_STANDARD_PART_2",
+      "OCCUPATION_STANDARD_PART_3",
+      "OCCUPATION_STANDARD_PART_4",
+      "OCCUPATION_STANDARD_PART_5",
+      "OCCUPATION_STANDARD_PART_6",
+    
+      # Raw
+      "TITLE_AS_TRANSCRIBED",
+      "CIVIL_STATUS"
+    )
+  ) %>% 
+  select(SOURCE, ID_I, TYPE, YEAR, VALUE) %>% 
+  pivot_wider(names_from = TYPE, values_from = VALUE)
 
+# data0 %>% 
+#   drop_na(OCCUPATION_STANDARD_PART_1) %>% View()
 
 # ==== Cleaning data0 ====
 data0 = data0 %>%
