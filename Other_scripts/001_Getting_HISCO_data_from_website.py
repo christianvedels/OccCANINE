95f57ada06db3dce338b0295e99da58cd4d9b4c3
christianvedels <christian-vs@sam.sdu.dk>
2023-11-13 16:50:20 +0100

    Update 001_Getting_HISCO_data_from_website.py

--- Other_scripts/001_Getting_HISCO_data_from_website.py
+++ Other_scripts/001_Getting_HISCO_data_from_website.py
@@ -186,8 +186,101 @@ def get_one(i, sleep_max = 3, sleep_min = 1, save_interval=100):
             
             return 0
         
+def get_one_again(i, sleep_max = 3, sleep_min = 1, save_interval=100):
+    # Second run
+    # breakpoint()
+    # ==== Setup ====    
+    id0 = i
+    url = f"https://historyofwork.iisg.nl/detail_hiswi.php?know_id={id0}&lang="
+    
+    # ==== Check if checked =====
+    
+    if os.path.isfile(f"../Data/Raw_data/HISCO_website/Many_files/{id0}.csv"):
+        print(f"{i} has already been scraped.")
+        if id0 in to_rescrape['id'].tolist():
+            print("-->But it is on rescrape list")
+        else:
+            return 0
+    
+    # Check if 'i' has already been checked
+    if id0 in checked_i_values:
+        print(f"{i} has already been checked to be empty.")
+        if id0 in to_rescrape['id'].tolist():
+            print("-->But it is on rescrape list")
+        else:
+            return 0
+    
+    # ==== Get response ====
+    print(f"Trying {i}: {url}")
+        
+    response = requests.get(url)
+    sleep(randint(sleep_min, sleep_max))
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find the table in the HTML content
+    table = soup.find('table')
+        
+    # Create an empty list to store the table data
+    table_data = []
+    
+    # Extract the data from each row of the table
+    for row in table.find_all('tr'):
+        row_data = []
+        for cell in row.find_all(['th', 'td']):
+            row_data.append(cell.text.strip())
+        table_data.append(row_data)
+        
+    # Find 'Occupational title'
+    index_with_occupational_title = [i for i, sublist in enumerate(table_data) if 'Occupational title' in sublist]
+    
+    if len(index_with_occupational_title)==0:
+        print(f'Found no occupational title in {i}: {url}')
+        return 0
+    
+    # Check when tupple
+    start_index = [i for _, i in enumerate(index_with_occupational_title) if len(table_data[i]) in [2, 3]]
+    
+    table_data = table_data[start_index[0]:]
+    
+    # Delete 'column' element, which is empty:
+    if len(table_data ) > 0 and len(table_data[0]) > 1:
+        del table_data[0][1]
+    
+    # ==== Housekeeping ====
+    # Update progress saver
+    # Add 'i' to the set of checked values
+    checked_i_values.add(id0)
+    
+    # Check if it's time to save checked_i_values
+    if len(checked_i_values) % save_interval == 0:
+        save_checked_values(checked_i_values)
+    
+    if(len(table_data)<8):
+        print(f'Skipped id {id0}')
+        return 0
+        
+    # ==== Process and save results =====
+        
+    # Create a pandas DataFrame from the table data
+    df = pd.DataFrame(table_data, columns = ["Type", "Content"])
             
+    # Save file 
+    fname = f"../Data/Raw_data/HISCO_website/Many_files/{id0}.csv"
+    df.to_csv(fname)
+    
+    print(f"Found data in id {id0}")
+    print(df['Content'][0])
+    
+    return 1
 
 # %% Run it
+# for i in range(7722, 100000):
+#     get_one(i)
+
+
+    
+# %% Run it again 
+to_rescrape = pd.read_csv("../Data/Raw_data/HISCO_website/To_rescrape.csv")
 for i in range(7722, 100000):
-    get_one(i)
+    get_one_again(i)
