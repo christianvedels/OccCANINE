a1312b381e8c0125d7c0b29072ae1e659cd3bc1b
christianvedels <christian-vs@sam.sdu.dk>
2023-11-13 22:51:27 +0100

    Misc updates

--- Data_cleaning_scripts/016_Titles_from_HISCO_website.R
+++ Data_cleaning_scripts/016_Titles_from_HISCO_website.R
@@ -2,7 +2,9 @@
 # Created:    2023-11-09
 # Auhtors:    Christian Vedel [christian-vs@sam.sdu.dk],
 #
-# Purpose:    This script scrapes and cleans occupational titles from historyofwork.iisg.nl
+# Purpose:    This cleans occupational titles from historyofwork.iisg.nl
+#             The underlying data is scraped using: 
+#                 'Other_scripts/001_Getting_HISCO_data_from_website.py'
 #
 # Output:     Clean tmp version of the data
 
@@ -80,27 +82,30 @@ data0 %>%
   distinct(occ1, Language, Country, hisco_1) %>% 
   NROW()
 
+# Manual look at some data
+data0 %>% filter(Provenance == 97125) %>% select(url) %>% unlist()
+data0 %>% filter(Provenance == 94990) %>% select(url) %>% unlist()
 
 
 # ==== To rescrape ====
-pt1 = data0 %>% 
-  filter(is.na(occ1)) %>% 
-  select(id)
-
-# Running numbers
-skipping_ids = data0 %>% 
-  mutate(dif = as.numeric(id) - dplyr::lag(as.numeric(id))) %>% 
-  filter(dif != 1) %>% select(id, dif)
-
-skipped_ids = foreach(i = 1:NROW(skipping_ids)) %do% {
-  skipping_ids$id[i] - seq(1, skipping_ids$dif[i]-1)
-} %>% unlist()
-
-skipped_ids %>% length()
-
-to_rescrape = data.frame(id = c(pt1$ids, skipped_ids))
-
-to_rescrape %>%
-  write_csv2("Data/Raw_data/HISCO_website/To_rescrape.csv")
+# pt1 = data0 %>% 
+#   filter(is.na(occ1)) %>% 
+#   select(id)
+# 
+# # Running numbers
+# skipping_ids = data0 %>% 
+#   mutate(dif = as.numeric(id) - dplyr::lag(as.numeric(id))) %>% 
+#   filter(dif != 1) %>% select(id, dif)
+# 
+# skipped_ids = foreach(i = 1:NROW(skipping_ids)) %do% {
+#   skipping_ids$id[i] - seq(1, skipping_ids$dif[i]-1)
+# } %>% unlist()
+# 
+# skipped_ids %>% length()
+# 
+# to_rescrape = data.frame(id = c(pt1$ids, skipped_ids))
+# 
+# to_rescrape %>%
+#   write_csv2("Data/Raw_data/HISCO_website/To_rescrape.csv")
 
 
