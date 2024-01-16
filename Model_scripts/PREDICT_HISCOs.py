374c93bd8664cb7405484f384c08b0ad3c7ad9b4
christianvedels <christian-vs@sam.sdu.dk>
2024-01-16 01:17:26 +0100

    Update PREDICT_HISCOs.py

--- Model_scripts/PREDICT_HISCOs.py
+++ Model_scripts/PREDICT_HISCOs.py
@@ -3,6 +3,8 @@
 Created on 2024-01-15
 
 @author: christian-vs
+
+Prediction for applications
 """
 
 import os
@@ -10,15 +12,15 @@ script_directory = os.path.dirname(os.path.abspath(__name__))
 os.chdir(script_directory)
 
 # %%
-# Specify files
-file_path = "Deliveries/230522 Human captial in the Nordics/Occupations_dk.csv"
-output_path = "Deliveries/230522 Human captial in the Nordics/Predictions"
+# Input / output relative to "Model_scripts"
+file_path = "../Deliveries/230522 Human captial in the Nordics/Occupations_dk.csv"
+output_path = "../Deliveries/230522 Human captial in the Nordics/Predictions"
 
 # Choose which GPU to use
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 MODEL_DOMAIN = "Multilingual_CANINE" # Type of model
-BATCH_SIZE = 256 # Adjust this down 
+BATCH_SIZE = 256 # Adjust this down if GPU cannot handle it. Adjust it up otherwise
 
 # Name of the finetuned model to use (must be located in "Trained_models") 
 model_name = "CANINE_Multilingual_CANINE_sample_size_10_lr_2e-05_batch_size_256"
@@ -26,8 +28,8 @@ model_name = "CANINE_Multilingual_CANINE_sample_size_10_lr_2e-05_batch_size_256"
 # %% Import necessary modules
 import torch
 import pandas as pd
-from Model_scripts.n103_Prediction_assets import Finetuned_model
-from Model_scripts.n102_DataLoader import Load_val
+from n103_Prediction_assets import Finetuned_model
+from n102_DataLoader import Load_val
 
 #%% Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
