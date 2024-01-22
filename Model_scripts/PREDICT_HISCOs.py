3a8dbee601e7ad9d7eb56cba2031e156ae9fba92
christianvedels <christian-vs@sam.sdu.dk>
2024-01-22 10:14:18 +0100

    Simplified PREDICT_HISCOs

--- Model_scripts/PREDICT_HISCOs.py
+++ Model_scripts/PREDICT_HISCOs.py
@@ -12,22 +12,18 @@ script_directory = os.path.dirname(os.path.abspath(__name__))
 os.chdir(script_directory)
 
 # %% Import necessary modules
-import torch
 from n103_Prediction_assets import Finetuned_model
-
-#%% Device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # %% Load model
 model = Finetuned_model(
-    model_name = "CANINE_Multilingual_CANINE_sample_size_10_lr_2e-05_batch_size_256", 
-    device = device, 
+    name = "CANINE_Multilingual_CANINE_sample_size_10_lr_2e-05_batch_size_256", 
+    device = "cuda", 
     batch_size = 256, 
     verbose = True
     )
 
 # %% Use the model
 model.predict(
-    ["taylor of fne dresses"],
+    ["taylor of fne dresses and mechanic of motor vehicles"],
     what = "pred"
     )
