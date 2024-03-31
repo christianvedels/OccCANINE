8ecd31ec7eba7ab6f5695cb3f0216e499ef8ec97
christianvedels <christian-vs@sam.sdu.dk>
2024-03-31 15:05:32 +0200

    Update PREDICT_HISCOs.py

--- PREDICT_HISCOs.py
+++ PREDICT_HISCOs.py
@@ -4,55 +4,29 @@ Created on 2024-01-15
 
 @author: christian-vs
 
-Prediction for applications
-
 SETUP:
-    - See readme2.md
+    - See GETTING_STARTED.md (https://github.com/christianvedels/OccCANINE/blob/main/GETTING_STARTED.md)
 """
 
-import os
-script_directory = os.path.dirname(os.path.abspath(__name__))
-os.chdir(script_directory)
-
-# %% Import necessary modules
-from OccCANINE.n103_Prediction_assets import OccCANINE
-
-# %% Load model
-model = OccCANINE()
-
-# %% Example 1
-model.predict(
-    ["tailor of the finest suits"], 
-    lang = "en", 
-    get_dict = True, 
-    threshold = 0.22 # Best F1 for English
-    )
-
-# %% Example 2
-model.predict(
-    ["the train's fireman"], 
-    lang = "en", 
-    get_dict = True, 
-    threshold = 0.22 # Best F1 for English
-    )
-
-# %% Example 3
-model.predict(
-    ["nurse at the local hospital"],
-    lang = "en", 
-    get_dict = True, 
-    threshold = 0.22 # Best F1 for English
-    )
-
-# %% Example 4 - 10000 (and more) are still very fast
+# Import necessary modules
+from histocc  import OccCANINE
 import pandas as pd
-df = pd.read_csv("Data/TOYDATA.csv")
-model.verbose = True # Set updates to True
-x = model.predict(
-    df["occ1"],
-    lang = "en",
-    threshold = 0.22
+
+# Load model
+model = OccCANINE(
+    verbose = True # To see updates while running prediction
     )
 
-x["occ1"] = df["occ1"]
-x.to_csv("Data/TOYDATA_wHISCO.csv")
+if __name__ == '__main__':
+    # Load data
+    df = pd.read_csv("histocc/Data/TOYDATA.csv")
+
+    # Get HISCO codes
+    result = model.predict(
+        df["occ1"],
+        lang = "en",
+        threshold = 0.22 # Optimal for F1 in English (see the paper https://arxiv.org/abs/2402.13604)
+        )
+    
+    # Save results
+    result.to_csv("Tmp.csv")
