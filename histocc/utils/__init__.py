af7bd2f68d3de7587ce48ea5f4a7b3aec279867b
christianvedels <40788950+christianvedels@users.noreply.github.com>
2024-07-09 15:44:17 +0200

    Revert "Entrypoint for seq2seq-style model training"

--- histocc/utils/__init__.py
+++ histocc/utils/__init__.py
@@ -2,6 +2,4 @@ from .masking import create_mask
 from .metrics import (
     Averager,
     seq2seq_sequence_accuracy,
-    order_invariant_accuracy,
 )
-from .log_util import wandb_init, update_summary
