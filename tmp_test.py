57485065eb571602f7f871bc7a9ab02ec273e109
christianvedels <christian.vedel.soerensen@gmail.com>
2024-09-26 09:55:33 +0200

    Update tmp_test.py

--- tmp_test.py
+++ tmp_test.py
@@ -9,16 +9,16 @@ from histocc.prediction_assets import OccCANINE
 import pandas as pd
 
 if __name__ == "__main__":
-    mod = OccCANINE(name = "Data/models/mixer-s2s=10-s=1605000.bin", hf = False, batch_size = 256, verbose = True)
-    # mod = OccCANINE(name = "Data/models/baseline-s=1600000.bin", hf = False)
+    # mod = OccCANINE(hf = True, batch_size = 256, verbose = True)
+    # mod = OccCANINE(name = "Data/models/mixer-s2s=10-s=1605000.bin", hf = False, batch_size = 16, verbose = True)
+    mod = OccCANINE(name = "Data/models/baseline-s=1600000.bin", hf = False)
     # mod = OccCANINE(name = "Data/models/flat_occCANINE.bin", hf = False, verbose = True, batch_size = 128)
     
     # print(mod.predict(["he is a farmer", "he is a fisherman"], behavior="fast", what = "probs"))
     # print(mod.predict_old(["he is a farmer"]))
     
-    
     toydata = pd.read_csv("histocc/Data/TOYDATA.csv")
-    res = mod.predict(toydata.occ1.tolist()[0:100], lang = "en", behavior="good", what = "pred")
+    res = mod.predict(toydata.occ1.tolist()[0:100], lang = "en", what = "pred")
     print(res)
     # print(mod.predict_old(toydata.occ1.tolist()[0:1000]))
     
