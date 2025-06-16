464522cf445c0911b281dc5032ba1bbc474fd520
christianvedels <christian.vedel.soerensen@gmail.com>
2025-06-16 20:41:25 +0200

    Update predict_ood.py

--- Project_dissemination/Paper_replication_package/Model_eval_python/predict_ood.py
+++ Project_dissemination/Paper_replication_package/Model_eval_python/predict_ood.py
@@ -4,48 +4,53 @@ from histocc.eval_metrics import EvalEngine
 import pandas as pd
 import os
 
-mod = OccCANINE()
+def main():
+    mod = OccCANINE()
 
-# list files
-files = os.listdir('Data/OOD_data')
+    # list files
+    files = os.listdir('Data/OOD_data')
 
-for f in files:
-    if f == 'Predictions':
-        continue
-    print(f'------> Predicting {f}')
-    data_f = pd.read_csv(f'Data/OOD_data/{f}')
+    for f in files:
+        if f == 'Predictions':
+            continue
+        print(f'------> Predicting {f}')
+        data_f = pd.read_csv(f'Data/OOD_data/{f}')
 
-    res = mod(data_f.occ1.tolist(), lang = f[0:2].lower())
+        res = mod(data_f.occ1.tolist(), lang = f[0:2].lower())
 
-    eval_engine = EvalEngine(mod, ground_truth = data_f, predicitons = res, pred_col = "hisco_")
-    res[f"acc"] = eval_engine.accuracy(return_per_obs = True)
-    res["precision"] = eval_engine.precision(return_per_obs = True)
-    res["recall"] = eval_engine.recall(return_per_obs = True)
-    res["f1"] = eval_engine.f1(return_per_obs = True)
+        eval_engine = EvalEngine(mod, ground_truth = data_f, predicitons = res, pred_col = "hisco_")
+        res[f"acc"] = eval_engine.accuracy(return_per_obs = True)
+        res["precision"] = eval_engine.precision(return_per_obs = True)
+        res["recall"] = eval_engine.recall(return_per_obs = True)
+        res["f1"] = eval_engine.f1(return_per_obs = True)
 
-    # Add in rowid
-    res["rowid"] = data_f.RowID
-
-    # Print
-    print(f"    Acc: {eval_engine.accuracy()}")
-    print(f"    Precision: {eval_engine.precision()}")
-    print(f"    Recall: {eval_engine.recall()}")
-    print(f"    F1: {eval_engine.f1()}")
-
-    # Eval digit by digit
-    for digits in range(1, 6): 
-        print(f"Digits: {digits}")
-        eval_engine = EvalEngine(mod, ground_truth = data_f, predicitons = res, pred_col = "hisco_", digits=digits)
-        res[f"acc_{digits}"] = eval_engine.accuracy(return_per_obs = True)
-        res[f"precision_{digits}"] = eval_engine.precision(return_per_obs = True)
-        res[f"recall_{digits}"] = eval_engine.recall(return_per_obs = True)
-        res[f"f1_{digits}"] = eval_engine.f1(return_per_obs = True)
+        # Add in rowid
+        res["rowid"] = data_f.RowID
 
         # Print
-        print(f"    Acc (digits: {digits}): {eval_engine.accuracy()}")
-        print(f"    Precision (digits: {digits}): {eval_engine.precision()}")
-        print(f"    Recall (digits: {digits}): {eval_engine.recall()}")
-        print(f"    F1 (digits: {digits}): {eval_engine.f1()}")
+        print(f"    Acc: {eval_engine.accuracy()}")
+        print(f"    Precision: {eval_engine.precision()}")
+        print(f"    Recall: {eval_engine.recall()}")
+        print(f"    F1: {eval_engine.f1()}")
 
-    # Save predictions
-    res.to_csv(f'Data/OOD_data/Predictions/predictions_{f}')
+        # Eval digit by digit
+        for digits in range(1, 6): 
+            print(f"Digits: {digits}")
+            eval_engine = EvalEngine(mod, ground_truth = data_f, predicitons = res, pred_col = "hisco_", digits=digits)
+            res[f"acc_{digits}"] = eval_engine.accuracy(return_per_obs = True)
+            res[f"precision_{digits}"] = eval_engine.precision(return_per_obs = True)
+            res[f"recall_{digits}"] = eval_engine.recall(return_per_obs = True)
+            res[f"f1_{digits}"] = eval_engine.f1(return_per_obs = True)
+
+            # Print
+            print(f"    Acc (digits: {digits}): {eval_engine.accuracy()}")
+            print(f"    Precision (digits: {digits}): {eval_engine.precision()}")
+            print(f"    Recall (digits: {digits}): {eval_engine.recall()}")
+            print(f"    F1 (digits: {digits}): {eval_engine.f1()}")
+
+        # Save predictions
+        res.to_csv(f'Data/OOD_data/Predictions/predictions_{f}')
+        
+if __name__ == "__main__":
+    main()
+
