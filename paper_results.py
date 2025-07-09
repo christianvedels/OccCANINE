50584fa99486f4f3682cbfcb0ead604adffd9892
christianvedels <christian.vedel.soerensen@gmail.com>
2025-07-09 14:26:35 +0200

    Renamed - this file is the recipe for paper results (via python - some plots, etc. in R)

---
+++ paper_results.py
@@ -0,0 +1,8 @@
+from Project_dissemination.Paper_replication_package.Model_eval_python.predict_ood import main as predict_ood_main
+from Project_dissemination.Paper_replication_package.Model_eval_python.finetune import main as finetune_main
+# from Project_dissemination.Paper_replication_package.Model_eval_python.predict_testbylang import main
+
+if __name__ == "__main__":
+    predict_ood_main()
+    # finetune_main()
+
