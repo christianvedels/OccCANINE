fe76a30d6b97148e2f8a44f8dc18de4d0a239080
christianvedels <christian.vedel.soerensen@gmail.com>
2025-07-25 22:35:18 +0200

    Test by lang update

--- Project_dissemination/Paper_replication_package/Model_eval_python/predict_testbylang.py
+++ Project_dissemination/Paper_replication_package/Model_eval_python/predict_testbylang.py
@@ -2,6 +2,8 @@ from histocc import OccCANINE, EvalEngine
 import pandas as pd
 from histocc.prediction_assets import THRESHOLD_LOOKUP
 import glob
+import json
+import os
 
 def load_data(n_obs=5000, data_path="Data/Test_data/*.csv", lang = None):
     """
@@ -19,7 +21,7 @@ def load_data(n_obs=5000, data_path="Data/Test_data/*.csv", lang = None):
     if lang:
         df = df[df["lang"] == lang]
 
-    df = df.sample(n=n_obs, random_state=10) if n_obs < df.shape[0] else df
+    df = df.sample(n=n_obs, random_state=20) if n_obs < df.shape[0] else df
     df = df.reset_index(drop=True)
     return df
 
@@ -36,14 +38,20 @@ def run_eval(df, mod, prediction_type, lang, thr=0.31, digits=5):
     Returns:
         pd.DataFrame: DataFrame with evaluation results.
     """
+    # Test if file exists
+    dummy_files = f"Project_dissemination/Paper_replication_package/Data/test_performance/lang/test_performance_{prediction_type}_digits_5_lang_{lang}.csv"
+    if os.path.exists(dummy_files):
+        print(f"Skipping {dummy_files} as it already exists.")
+        return
+
     if prediction_type == "full":
-        # Downsample to 10k because its slow
-        df = df.sample(n=500, random_state=20) if df.shape[0] > 500 else df
+        # Downsample to 1k because its slow
+        df = df.sample(n=1000, random_state=20) if df.shape[0] > 1000 else df
 
     preds = mod(
         df["occ1"].tolist(), 
         df["lang"].tolist(), 
-        threshold=0.31, 
+        threshold=thr, 
         prediction_type=prediction_type, 
         deduplicate=True
     )
@@ -51,7 +59,7 @@ def run_eval(df, mod, prediction_type, lang, thr=0.31, digits=5):
     preds_unk = mod(
         df["occ1"].tolist(), 
         "unk", 
-        threshold=0.31, 
+        threshold=thr, 
         prediction_type=prediction_type, 
         deduplicate=True
     )
@@ -86,6 +94,9 @@ def run_eval(df, mod, prediction_type, lang, thr=0.31, digits=5):
         }])
 
         file = f"Project_dissemination/Paper_replication_package/Data/test_performance/lang/test_performance_{prediction_type}_digits_{d}_lang_{lang}.csv"
+        # Make dir if missing
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        # Save results
         res.to_csv(file, index=False)
         print(f"Results saved to {file}")
         print(res)
@@ -106,24 +117,41 @@ def run_eval(df, mod, prediction_type, lang, thr=0.31, digits=5):
         print(f"Results saved to {file_unk}")
         print(res_unk)
 
-def main():
+def main(toyrun=False):
     """
     Main function to load data, get predictions, and evaluate the model.
     """
     # Load the model
     mod = OccCANINE()
 
+    # Read the threshold lookup
+    if not os.path.exists("Project_dissemination/Paper_replication_package/Data/Intermediate_data/thresholds_by_lang.json"):
+        raise FileNotFoundError(
+            "Threshold lookup file not found. Please run threshold tuning first."
+        )
+    
+    with open("Project_dissemination/Paper_replication_package/Data/Intermediate_data/thresholds_by_lang.json", "r") as f:
+        THRESHOLD_LOOKUP = json.load(f)
+
     # Load data
     unique_langs = list(THRESHOLD_LOOKUP.keys())
     for lang in unique_langs:
+        if lang in ['overall']:
+            continue # Skip overall as it is not a language
+
         print(f"Performing test for {lang}")
         
-        df = load_data(n_obs=10000, data_path="Data/Test_data/*.csv", lang=lang)
+        if toyrun:
+            df = load_data(n_obs=100, data_path="Data/Test_data/*.csv", lang=lang)
+        else:
+            # Load full data for production run
+            df = load_data(n_obs=10000, data_path="Data/Test_data/*.csv", lang=lang)
         
         # Run evaluations for different prediction types
-        run_eval(df, mod, prediction_type="flat", lang=lang, thr=0.31)
-        run_eval(df, mod, prediction_type="greedy", lang=lang, thr=0.31)
-        # run_eval(df, mod, prediction_type="full", lang=lang, thr=0.25) # Drop full for now, its slow
+        run_eval(df, mod, prediction_type="flat", lang=lang, thr=THRESHOLD_LOOKUP.get(lang).get("flat"))
+        run_eval(df, mod, prediction_type="greedy", lang=lang, thr=99) # Placeholder threshold (not used in greedy)
+        if not toyrun: # Only run full for production
+            run_eval(df, mod, prediction_type="full", lang=lang, thr=THRESHOLD_LOOKUP.get(lang).get("full"))
         print(f"Completed evaluations for {lang}")
 
 if __name__ == "__main__":
